9dea0c1619125139f158a0b55f6556f48f0f147a
Stephanie Cabrera <stephanie.cabrera.333@gmail.com>
2021-08-06 15:34:31 -0700

    Delete functionality works and ids were fixed

--- Express.js
+++ Express.js
@@ -23,31 +23,17 @@ app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html'
 //Getting the notes.html page
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
 
-app.delete('api/notes/:id', (req, res) => {
-    const id = req.params.id;
-    console.log(id);
-    fs.readFile('./db/db.json', 'UTF-8', (err, data) => {
-        if(err)throw err;
-        
-        let jsonData = JSON.parse(data);
-        jsonData.splice(id, 1);
-        fs.writeFile('./db/db.json', JSON.stringify(jsonData, null, 2), (err, data) => {
-            if(err)throw err;
-             console.log('Note deleted');
-             //console.log(data);
-            return res.send(data);
-        });
-        
-    })
-});
-
 //Read the data from the db.json
 app.get('/api/notes', (req, res) => {
     fs.readFile('./db/db.json', 'utf8', (err, data) => {
         if(err){throw err}
-        //let newData = JSON.parse(data);
-        //console.log(newData);
-        return res.send(data);
+        let newData = JSON.parse(data);
+        console.log(newData);
+        newData.forEach((element, id) => {
+            element.id = id + 1;
+        });
+        console.log(newData);
+        return res.send(newData);
     });
 });
 
@@ -56,8 +42,8 @@ app.post('/api/notes', (req, res) => {
     console.log(req.body);
     fs.readFile('./db/db.json', 'UTF-8', (err, data) => {
        if(err)throw err;
-       
-       let jsonData = JSON.parse(data);
+       let jsonData;
+       (data != undefined) ? jsonData = JSON.parse(data) : jsonData = Array(0);
 
        let userData = {
           title: `${req.body.title}`,
@@ -65,12 +51,39 @@ app.post('/api/notes', (req, res) => {
        };
 
        jsonData.push(userData);
+
        fs.writeFile('./db/db.json', JSON.stringify(jsonData, null, 2), (err, data) => {
             if(err)throw err;
              console.log('Note saved');
-             //console.log(data);
+             console.log(data);
+            userData.id = jsonData.length;
+            console.log(`userData ${userData}`);
+            return res.send(userData);
         });
-        return res.send(userData);
+        
+    })
+});
+
+app.delete('/api/notes/:id/', (req, res) => {
+    const id = req.params.id;
+    console.log(id);
+    fs.readFile('./db/db.json', 'UTF-8', (err, data) => {
+        if(err)throw err;
+        
+        let jsonData = JSON.parse(data);
+        jsonData.splice(id - 1, 1);
+        fs.writeFile('./db/db.json', JSON.stringify(jsonData, null, 2), (err, data) => {
+            if(err)throw err;
+             console.log('Note deleted');
+             console.log(data);
+            
+        });
+
+        jsonData.forEach((element, id) => {
+            element.id = id + 1;
+        });
+        return res.send(jsonData);
+        
     })
 });
 
